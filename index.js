8c3a889cd6edec6b7fa389bc15d1c332ad63a248
yigittopm <yigittopm@hotmail.com>
2022-06-09 12:06:37 +0300

    connect db and set config

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Mongodb url
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+// Connect to mongodb with mongoose
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Successfully connect to mongodb.."))
+    .catch((err) => {
+      console.log(err);
+      // Retry connect to db after 4000 milisecond
+      setTimeout(connectWithRetry, 4000);
+    });
+};
+
+connectWithRetry();
+
+// Home endpoint
 app.get("/", (req, res) => {
   res.send(`
         <h1>
